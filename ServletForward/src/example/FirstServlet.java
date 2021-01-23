5a470c232b674bb18b3cb5cc2fae98bd658c7ef7
Dev-RubinJo <dbqlsdl1234@naver.com>
2021-01-23 14:16:34 +0900

    Study about Javacript locaion.href with Servlet

--- ServletForward/src/example/FirstServlet.java
+++ ServletForward/src/example/FirstServlet.java
@@ -6,13 +6,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/first")
 public class FirstServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter printWriter = resp.getWriter();
         resp.setContentType("text/html;charset=utf-8");
         resp.addHeader("Refresh", "1;url=second");
+        printWriter.print("<script type='text/javascript'>" +
+                "location.href='second';" +
+                "</script>");
     }
 }
