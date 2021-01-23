e85d9cae45ff0491a80e768f8b5252375dfd5da5
Dev-RubinJo <dbqlsdl1234@naver.com>
2021-01-23 13:52:07 +0900

    Study about Redirect with Servlet

---
+++ ServletForward/src/example/SecondServlet.java
@@ -0,0 +1,21 @@
+package example;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/second")
+public class SecondServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=utf-8");
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.print("<html><body>" +
+                "this is send redirect" +
+                "</body></html>");
+    }
+}
