53aed47f54905ce050ea1c5f325d3ba189e34497
Dev-RubinJo <dbqlsdl1234@naver.com>
2021-01-24 17:02:59 +0900

    RequestDispatcher를 이용한 데이터 전달 실습

--- ServletForward/src/example/FirstServlet.java
+++ ServletForward/src/example/FirstServlet.java
@@ -14,12 +14,7 @@ public class FirstServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/html;charset=utf-8");
-
-        PrintWriter writer = resp.getWriter();
-        writer.print("<script type='text/javascript'>" +
-                "location.href='second?name=Joo';" +
-                "</script>");
-//        writer.close();
+        RequestDispatcher dispatcher = req.getRequestDispatcher("second?name=Name");
+        dispatcher.forward(req, resp);
     }
 }
